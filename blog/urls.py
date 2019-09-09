8a06e4d08ce00f9c444676c5a3b13f6fbed32ff6
Kevin <chenchoguti.123@gmail.com>
2019-09-09 12:07:49 -0600

    Agregadas vistas y plantilla para el detalle del post del blog asi como css estatico

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     
     path('', views.listar_pub, name='listar_pub'),
+    path('pub/<int:pk>/', views.pub_detail, name='pub_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 
 def listar_pub(request):
 	pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
 	return render(request,'blog/listar_pub.html', {'pubs': pubs})
+
+def pub_detail(request, pk):
+	p = get_object_or_404(Publicacion, pk=pk)
+	return render(request, 'blog/pub_detail.html', {'p':p})
